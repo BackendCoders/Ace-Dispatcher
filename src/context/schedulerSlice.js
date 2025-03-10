889966937ad056d11af154e7b0f1eb9c9dc0e24a
TanyaSoni2912 <tanu139sona@gmail.com>
2025-03-10 10:30:20 +0530

    show completed by default true

--- src/context/schedulerSlice.js
+++ src/context/schedulerSlice.js
@@ -37,7 +37,7 @@ const schedulerSlice = createSlice({
 		currentlySelectedBookingIndex: -1,
 		selectedDriver: null,
 		activeDate: new Date().toISOString(),
-		activeComplete: isMobile ? true : false,
+		activeComplete: isMobile ? true : true,
 		activeAllocate: true,
 		activeSearch: false,
 		activeSoftAllocate: false,
